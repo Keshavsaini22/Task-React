66d97c5e86698af2df80a99691354c16451a0435
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-01-15 13:02:21 +0530

    ch1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import First from './components/first';
+
 import './App.css';
+import First from './components/first';
 
 function App() {
   const arr=[1,1,1,1,11,1,1,8]
   const obj={asD:'asd',asdasd:'123'}
   return (
       <>
-      {Object.keys(obj).map(()=>(
-        <First/>
+       <First/>
+      {/* {Object.keys(obj).map(()=>(
+      
 
-      ))}
+      ))} */}
       </>
   );
 }
