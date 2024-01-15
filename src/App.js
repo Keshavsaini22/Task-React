0b4895b8d9317e340b3ccc72cc76182034067db1
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-01-15 16:57:46 +0530

    new project shuru

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import First from './components/First';
 function App() {
   const arr = [1, 1, 1, 1, 11, 1, 1, 8]
   const obj = { asD: 'asd', asdasd: '123' }
-  return (
-    <>
-      <First />
-      {/* {Object.keys(obj).map(()=>(
+  {/* {Object.keys(obj).map(()=>(
       
 
       ))} */}
+  return (
+    <>
+      {/* <First /> */}
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      
     </>
   );
 }
--- src/components/first.jsx
+++ src/components/first.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
+    //PROPS <FIRST prop1="" prop2="">       this is during declaration
 
+
+//while defination 
+//function First(props){   props.prop1   props.prop2}
+//or function First({prop1,prop2}){   prop1   prop2}
 function First() {
   
   let [num,setnum]=useState(0);
@@ -9,7 +14,7 @@ function First() {
       if(num<20) num+=1;
       else num=0
       setnum(num)
-   }
+        }
    function handlesub(){
     if(num>0)   num-=1;
     else num=0;
@@ -20,7 +25,7 @@ function First() {
    <div>
     <button onClick={handleadd}> Press me to add</button>
     <button onClick={handlesub}> Press me to sub</button>
-    <h2> {num}</h2>
+    <h2 className='bg-green-400'> {num}</h2>
    </div>
   )
 }
